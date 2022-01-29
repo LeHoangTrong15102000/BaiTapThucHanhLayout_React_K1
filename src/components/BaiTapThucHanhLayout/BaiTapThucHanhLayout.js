ac9bd4bae5ebeffcd12f94190e9ec4222cbf3a74
Le Hoang Trong <langtupro0456@gmail.com>
2022-01-29 17:38:43 +0700

    Create source file React_Layout

---
+++ src/components/BaiTapThucHanhLayout/BaiTapThucHanhLayout.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class BaiTapThucHanhLayout extends Component {
+  render() {
+    return <div>
+        Bài tập thực hành layout
+    </div>;
+  }
+}
