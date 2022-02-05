cf6a669f5007ddcecb21504ae9b7660676509092
Le Hoang Trong <langtupro0456@gmail.com>
2022-02-05 11:36:33 +0700

    setState car

--- src/BaiTapChonXe/BaiTapChonXe.js
+++ src/BaiTapChonXe/BaiTapChonXe.js
@@ -3,12 +3,56 @@ import React, { Component } from "react";
 export default class BaiTapChonXe extends Component {
   // Việc đầu tiên trước khi làm reactjs là dàn layout trước
   // Sau này sẽ học về Sass
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: '../assets/products/black-car.jpg'
+    }
+  }
+
+
+  // Viêt cái hàm là changColor để thay đổi màu sắc của xe
+  changeColor = (color) => {
+    // console.log(color)
+    let imgSource = '';
+
+    switch(color) {
+      case 'red': {
+        imgSource = '../assets/products/red-car.jpg';
+      }; break;
+      
+      case 'steel': {
+        imgSource = '../assets/products/steel-car.jpg';
+      }; break;
+
+      case 'silver': {
+        imgSource = '../assets/products/silver-car.jpg';
+      }; break;
+
+      case 'black': {
+        imgSource = '../assets/products/black-car.jpg';
+      };break;
+    }
+
+
+    // Gọi phương thức setState để truyền imgSource vào mới từ sự kiện click của button => render lại giao diện với source mới
+    this.setState({
+      img: imgSource
+    })
+    
+  }
   render() {
     // Nếu cái hình ở trong cái thư mục của chúng ta thì chèn vào như sau
     // src={require} thêm vào chữ require
     // Còn Nếu tấm hình ở trên mạng lấy xuống thì chung ta để vào cặp dấu "" src={""}
 
     // Video tiếp theo sẽ hướng dẫn cách xử lý sự kiện trong js cho tấm hình đổi khi người dùng click vào tấm hình
+
+
+  
+
+
     return (
       <div className="container-fluid">
         <div className="row">
@@ -23,7 +67,7 @@ export default class BaiTapChonXe extends Component {
             <div className="card text-dark">
               <div className="card-header text-primary">Exterior Color</div>
               <div className="card-body">
-                <div className='row border border-line mb-2 pt-2 pb-2' onClick={() => {}} style={{cursor: 'pointer'}}>
+                <div className='row border border-line mb-2 pt-2 pb-2' onClick={() => this.changeColor('black')} style={{cursor: 'pointer'}}>
                 
                     <img className='col-2' style={{width: '100%'}} src={require('../assets/icons/icon-black.jpg')} alt='black_icon'/>
                    
@@ -33,7 +77,7 @@ export default class BaiTapChonXe extends Component {
                     </div>
                 </div>
 
-                <div className='row  border border-line mb-2 pt-2 pb-2' onClick={() => {}} style={{cursor: 'pointer'}}>
+                <div className='row  border border-line mb-2 pt-2 pb-2' onClick={() => this.changeColor('steel')} style={{cursor: 'pointer'}}>
                     
                     <img className='col-2' style={{width: '100%'}} src={require('../assets/icons/icon-steel.jpg')} alt='steel_icon'/>
                     
@@ -43,7 +87,7 @@ export default class BaiTapChonXe extends Component {
                     </div>
                 </div>
 
-                <div className='row  border border-line mb-2 pt-2 pb-2' onClick={() => {}} style={{cursor: 'pointer'}}>
+                <div className='row  border border-line mb-2 pt-2 pb-2' onClick={() => this.changeColor('silver')} style={{cursor: 'pointer'}}>
                     <img className='col-2' style={{width: '100%'}} src={require('../assets/icons/icon-silver.jpg')} alt='silver_icon'/>
                    
                     <div className='col-10'>
@@ -52,7 +96,7 @@ export default class BaiTapChonXe extends Component {
                     </div>
                 </div>
 
-                <div className='row  border border-line mb-2 pt-2 pb-2' onClick={() => {}} style={{cursor: 'pointer'}}>
+                <div className='row  border border-line mb-2 pt-2 pb-2' onClick={() => this.changeColor('red')} style={{cursor: 'pointer'}}>
                     
                     <img className='col-2' style={{width: '100%'}} src={require('../assets/icons/icon-red.jpg')} alt='red_icon'/>
                     
