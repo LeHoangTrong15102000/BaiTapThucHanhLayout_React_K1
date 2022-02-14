0cc7b582c5282b5156bc9187ce94fe4879027adc
Le Hoang Trong <langtupro0456@gmail.com>
2022-02-14 16:51:46 +0700

    Create SelectCar file

---
+++ src/SelectCar/SelectCar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class SelectCar extends Component {
+  // Trong bài tập này sẽ giàn layout và code chức năng chọn xe cho bài tập xe
+
+  // Cho mảng dữ liệu xây dựng ứng dụng chọn xe.
+  // Thực hiện chức năng chọn màu xe
+  // Thực hiện chức năng chọn bánh xe
+  // Thực hiện chức năng xoay xe sử dụng thư viện js-cloudimage-360-view
+  render() {
+    return <div>SelectCar</div>;
+  }
+}
