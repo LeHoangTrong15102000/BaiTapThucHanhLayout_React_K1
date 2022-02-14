867006fccdfa2d2506e3acdba8fddf0c5f55f4d9
Le Hoang Trong <langtupro0456@gmail.com>
2022-02-14 21:49:41 +0700

    Complete Carousel Section

--- src/Exercise2/Carousel.jsx
+++ src/Exercise2/Carousel.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import PhoneImage from '../assets/image/product-header2.png'
+import CarouselBackground from '../assets/image/product-header-bg.jpg'
+import './Carousel.scss'
 
 export default class Carousel extends Component {
+
+    // Bắt buộc phải viết backgroundImage vào bên trong thẻ section
   render() {
     return (
-      <section className="carousel">
+      <section style={{backgroundImage: `url(${CarouselBackground})`, width: '100%'}} className="carousel">
         <div className="container">
             <div className="content">
                 <h1>CyberPhone</h1>
                 <span className="">Best SmartPhone of 2022</span>
                 <p className="">
-                    Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong
+                    Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong
                 </p>
 
                 <button>
