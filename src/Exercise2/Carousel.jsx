3d4232c84aea032e6277384c8eacfe4600234bdb
Le Hoang Trong <langtupro0456@gmail.com>
2022-02-21 19:26:46 +0700

    Complete Carousel section

--- src/Exercise2/Carousel.jsx
+++ src/Exercise2/Carousel.jsx
@@ -1,35 +1,42 @@
-import React, { Component } from 'react'
-import PhoneImage from '../assets/image/product-header2.png'
-import CarouselBackground from '../assets/image/product-header-bg.jpg'
-import './Carousel.scss'
+import React, { Component } from "react";
+import PhoneImage from "../assets/image/product-header2.png";
+import CarouselBackground from "../assets/image/product-header-bg.jpg";
+import "./Carousel.scss";
 
 export default class Carousel extends Component {
-
-    // Bắt buộc phải viết backgroundImage vào bên trong thẻ section
+  // Bắt buộc phải viết backgroundImage vào bên trong thẻ section
   render() {
     return (
-      <section style={{backgroundImage: `url(${CarouselBackground})`, width: '100%'}} className="carousel">
+      <section
+        style={{ backgroundImage: `url(${CarouselBackground})`, width: "100%" }}
+        className="carousel"
+      >
         <div className="container">
-            <div className="content">
-                <h1>CyberPhone</h1>
-                <span className="">Best SmartPhone of 2022</span>
-                <p className="">
-                    Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong
-                </p>
+          <div className="content">
+            <h1>CyberPhone</h1>
+            <span className="">Best SmartPhone of 2022</span>
+            <p className="">
+              Le Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong Le
+              Hoang Trong Le Hoang Trong Le Hoang Trong Le Hoang Trong
+            </p>
 
-                <button>
-                    <i class="fa fa-info-circle btn-info"></i> More Info
-                </button>
-                <button>
-                    <i class="fa fa-shopping-cart btn-shop"></i> Shop
-                </button>
+            <div className="mt-2">
+              <button className="btn-info">
+                More Info
+                <i className="fa fa-info-circle ml-1"></i>
+              </button>
+              <button className="btn-shop ml-2">
+                Shop
+                <i className="fa fa-shopping-cart ml-1"></i>
+              </button>
             </div>
+          </div>
 
-            <div className="thumbnail">
-                <img  src={PhoneImage} alt="PhoneCarousel"/>
-            </div>
+          <div className="thumbnail">
+            <img src={PhoneImage} alt="PhoneCarousel" />
+          </div>
         </div>
       </section>
-    )
+    );
   }
 }
